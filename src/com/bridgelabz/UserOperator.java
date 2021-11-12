d594a015dc48af5060ff8c73f85b2808effeacf7
renuka13599 <renuka13599@gmail.com>
2021-11-12 20:03:39 +0530

    Added:Program to check lastname is valid or not

--- src/com/bridgelabz/UserOperator.java
+++ src/com/bridgelabz/UserOperator.java
@@ -1,18 +1,23 @@
 package com.bridgelabz;
+/*Author:Renu
+*Date:12/11/21
+* Purpose:This program has to lastname is valid or not"
+ */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserOperator {
-    public void firstName(String firstName){
+    public void lastName(String lastName){
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(firstName);
+        Matcher matcher = pattern.matcher(lastName);
         boolean valid=matcher.matches();
         if (valid==true){
-            System.out.println("Your name "+firstName+" is Valid");
+            System.out.println("Your name "+lastName+" is Valid");
         }
         else{
-            System.out.println("Your name "+firstName+" is Invalid");
+            System.out.println("Your name "+lastName+" is Invalid");
         }
     }
+
 }
