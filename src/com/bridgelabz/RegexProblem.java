777fe5a0627e4000b82852579fc5281855a999cc
renuka13599 <renuka13599@gmail.com>
2021-11-12 20:47:22 +0530

    Added:Program to check atleast one uppercase  passrule2 is valid or not

--- src/com/bridgelabz/RegexProblem.java
+++ src/com/bridgelabz/RegexProblem.java
@@ -4,7 +4,7 @@ public class RegexProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to the User Registration Problem using Regex!!");
         UserOperator user = new UserOperator();
-        user.passrule1("Renukaece");//It is valid
+        user.passrule2("Renuka123");//It is valid
 
     }
 }
--- src/com/bridgelabz/UserOperator.java
+++ src/com/bridgelabz/UserOperator.java
@@ -1,22 +1,23 @@
 package com.bridgelabz;
 /*Author:Renu
 *Date:12/11/21
-* Purpose:This program has to pass rule1 is minimum 8 characters is valid or not"
+* Purpose:This program has to pass rule2 is atleast one uppercase is valid or not"
  */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserOperator {
-    public void passrule1(String passrule1){
-        String regex = "^[A-Z a-z 0-9]{9}$";
+    public void passrule2(String passrule2){
+       // String regex = "^[A-Z a-z 0-9]{9}$";
+        String regex = "^(?=.*[A-Z]){1}(?=.*[a-z]{8})$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(passrule1);
+        Matcher matcher = pattern.matcher(passrule2);
         boolean valid=matcher.matches();
         if (valid==true){
-            System.out.println("Your name "+passrule1+" is Valid");
+            System.out.println("Your password "+passrule2+" is Valid");
         }
         else{
-            System.out.println("Your name "+passrule1+" is Invalid");
+            System.out.println("Your password "+passrule2+" is Invalid");
         }
     }
 
