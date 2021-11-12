fb8cfed462438feb576bb44c636a040afe14b094
renuka13599 <renuka13599@gmail.com>
2021-11-12 19:19:05 +0530

    Added:Program to check firstname is valid or not

---
+++ src/com/bridgelabz/RegexProblem.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class RegexProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration Problem using Regex!!");
+        UserOperator user = new UserOperator();
+        user.firstName("Renuka");
+
+    }
+}
