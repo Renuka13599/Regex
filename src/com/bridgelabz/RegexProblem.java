df4700cafc81b05f3da7e5e8bb9bf02df6446ff4
renuka13599 <renuka13599@gmail.com>
2021-11-12 20:37:14 +0530

    Added:Program to check minimum 8 charcters passrule1

--- src/com/bridgelabz/RegexProblem.java
+++ src/com/bridgelabz/RegexProblem.java
@@ -4,7 +4,7 @@ public class RegexProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to the User Registration Problem using Regex!!");
         UserOperator user = new UserOperator();
-        user.mobileno("91 984678909");
+        user.passrule1("Renukaece");//It is valid
 
     }
 }
--- src/com/bridgelabz/UserOperator.java
+++ src/com/bridgelabz/UserOperator.java
@@ -1,22 +1,22 @@
 package com.bridgelabz;
 /*Author:Renu
 *Date:12/11/21
-* Purpose:This program has to mobile no is valid or not"
+* Purpose:This program has to pass rule1 is minimum 8 characters is valid or not"
  */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserOperator {
-    public void mobileno(String mobileno){
-        String regex = "^[0-9]{2} [6-9]{1}[0-9]{9}$";
+    public void passrule1(String passrule1){
+        String regex = "^[A-Z a-z 0-9]{9}$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(mobileno);
+        Matcher matcher = pattern.matcher(passrule1);
         boolean valid=matcher.matches();
         if (valid==true){
-            System.out.println("Your name "+mobileno+" is Valid");
+            System.out.println("Your name "+passrule1+" is Valid");
         }
         else{
-            System.out.println("Your name "+mobileno+" is Invalid");
+            System.out.println("Your name "+passrule1+" is Invalid");
         }
     }
 
