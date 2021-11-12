6f5e7bcec96d6fbc8bdc09f40c5696d16ddafd41
renuka13599 <renuka13599@gmail.com>
2021-11-12 20:25:38 +0530

    Added:Program to check mobileno is valid or not

--- src/com/bridgelabz/UserOperator.java
+++ src/com/bridgelabz/UserOperator.java
@@ -1,22 +1,22 @@
 package com.bridgelabz;
 /*Author:Renu
 *Date:12/11/21
-* Purpose:This program has to lastname is valid or not"
+* Purpose:This program has to mobile no is valid or not"
  */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserOperator {
-    public void email(String email){
-        String regex = "^[A-Z]{1}[- @ .][a-z]{2,}$";
+    public void mobileno(String mobileno){
+        String regex = "^[0-9]{2} [6-9]{1}[0-9]{9}$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(email);
+        Matcher matcher = pattern.matcher(mobileno);
         boolean valid=matcher.matches();
         if (valid==true){
-            System.out.println("Your name "+email+" is Valid");
+            System.out.println("Your name "+mobileno+" is Valid");
         }
         else{
-            System.out.println("Your name "+email+" is Invalid");
+            System.out.println("Your name "+mobileno+" is Invalid");
         }
     }
 
