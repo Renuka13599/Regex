80f4adda7056a3226975b550b24f4f9f38538fac
renuka13599 <renuka13599@gmail.com>
2021-11-12 21:14:04 +0530

    Added:Program to check provide email samples sepately

--- src/com/bridgelabz/RegexProblem.java
+++ src/com/bridgelabz/RegexProblem.java
@@ -4,7 +4,8 @@ public class RegexProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to the User Registration Problem using Regex!!");
         UserOperator user = new UserOperator();
-        user.passrule3("Renuka@23");//It is valid
+        user.passrule4("Renuka@23 Hemasiri#1213 Laddu@4546");//It is valid
 
     }
 }
+
--- src/com/bridgelabz/UserOperator.java
+++ src/com/bridgelabz/UserOperator.java
@@ -1,13 +1,13 @@
 package com.bridgelabz;
 /*Author:Renu
 *Date:12/11/21
-* Purpose:This program has to pass rule3 is atlest one special character in the password must be passed valid or not"
+* Purpose:This program has to seapartley passed email passed valid or not"
  */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserOperator {
-    public void passrule3(String password){
+    public void passrule4(String password){
        // String regex = "^[A-Z a-z 0-9]{9}$";
         String regex = "^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%!]).{8,}$";
         Pattern pattern = Pattern.compile(regex);
