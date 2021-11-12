1be0b9d6ca7fb3c3b8f241512662437471b5580c
renuka13599 <renuka13599@gmail.com>
2021-11-12 21:05:12 +0530

    Added:Program to check atleast one special charcter  passrule4 is valid or not

--- src/com/bridgelabz/UserOperator.java
+++ src/com/bridgelabz/UserOperator.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 /*Author:Renu
 *Date:12/11/21
-* Purpose:This program has to pass rule3 is atlest numeric number in the password must be passed valid or not"
+* Purpose:This program has to pass rule3 is atlest one special character in the password must be passed valid or not"
  */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 public class UserOperator {
     public void passrule3(String password){
        // String regex = "^[A-Z a-z 0-9]{9}$";
-        String regex = "^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9]).{8,}$";
+        String regex = "^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%!]).{8,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(password);
         boolean valid=matcher.matches();
